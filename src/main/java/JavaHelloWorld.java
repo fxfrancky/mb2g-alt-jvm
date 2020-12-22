eb85b8f31ab3b7c415246dce0f1b51b789923d83
Franck Owen <fxfrancky@yahoo.com>
2020-12-22 22:16:39 +0100

    Second groovy plugin commit

--- src/main/java/JavaHelloWorld.java
+++ src/main/java/JavaHelloWorld.java
@@ -1,6 +1,6 @@
 public class JavaHelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println(" Hello World !!!");
+    public String getHello() {
+        return " Hello World !!!";
     }
 }
